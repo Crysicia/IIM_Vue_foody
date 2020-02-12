0e2896f8c2464b6001f71f54756f83d03e99d552
Crysicia <7459685+Crysicia@users.noreply.github.com>
2020-02-12 09:12:52 +0100

    Add bootstrap

---
+++ src/components/HomePage.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h2>Home Page</h2>
+        <div class="jumbotron">azeazeaz</div>
+        <a class="btn btn-primary">azeazeae</a>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomePage'
+}
+</script>
+
+<style>
+</style>
